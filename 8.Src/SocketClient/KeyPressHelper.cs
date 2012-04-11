c1fec5df49fec93d875b1ca8a162395e490b7c94
hkiaipc <hkiaipc@163.com>
2012-04-11 09:51:31 +0800

    add KeyPressHelper.cs

---
+++ 8.Src/SocketClient/KeyPressHelper.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SocketClient
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class KeyPressHelper
+    {
+        internal static void Process(object sender, System.Windows.Forms.KeyPressEventArgs e)
+        {
+
+            char key = e.KeyChar;
+
+            if (char.IsDigit(key) ||
+                (key >= 'a' && key <= 'f') ||
+                (key >= 'A' && key <= 'F') ||
+                key == ' ')
+            {
+
+            }
+            else
+            {
+                e.KeyChar = (char)0;
+            }
+
+        }
+    }
+}
